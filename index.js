2716aa121ab12d16ed0776d05ec15220ac89d143
Abbie Cornelius <abbyc@sky.com>
2021-10-04 18:33:06 +0100

    commit tree 357829cdd8a4f157708796a6fad0573f5739bc9e

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Your code here
+function mapToNegativize(array) {
+    let updatedNumbers = []
+    for (let i = 0; i < array.length; i++) {
+        updatedNumbers.push(array[i] * -1)
+    }
+    return updatedNumbers
+}
+
+function mapToNoChange(array) {
+    return array
+}
+
+function mapToDouble(array) {
+    let updatedNumbers = []
+    for (let i = 0; i < array.length; i++) {
+        updatedNumbers.push(array[i] * 2)
+    }
+    return updatedNumbers
+}
+
+function mapToSquare(array) {
+    let updatedNumbers = []
+    for (let i = 0; i < array.length; i++) {
+        updatedNumbers.push(array[i] * array[i])
+    }
+    return updatedNumbers
+}
+
+
+function reduceToTotal(array, startingPoint=0) {
+    let total = startingPoint
+    for (let i = 0; i < array.length; i++ ) {
+      total = total + array[i]
+    }
+    return total
+}
+
+function reduceToAllTrue(array) {
+    for (let i = 0; i < array.length; i++ ) {
+      if (!array[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(array) {
+    for (let i = 0; i < array.length; i++ ) {
+      if (array[i]) return true
+    }
+    return false
+}
